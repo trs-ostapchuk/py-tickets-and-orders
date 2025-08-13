e89bdc58623e571da7fbdae99ba772e5b77bca05
Taras <tarasostapchuk27@gmail.com>
2025-08-13 20:13:50 +0200

    Create order.py with method create_order, get_orders

---
+++ services/order.py
@@ -0,0 +1,43 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.utils.dateparse import parse_datetime
+from db.models import Order, Ticket, MovieSession
+
+User = get_user_model()
+
+
+def create_order(tickets: list[dict], username: str, date: str = None) -> Order:
+
+    with transaction.atomic():
+        user = User.objects.get(username=username)
+
+        order_data = {"user": user}
+        if date:
+            parsed_date = parse_datetime(date)
+            if not parsed_date:
+                raise ValueError("Invalid date format. Use 'YYYY-MM-DD HH:MM'.")
+            order_data["created_at"] = parsed_date
+
+        order = Order.objects.create(**order_data)
+
+        tickets_objs = []
+        for ticket_data in tickets:
+            tickets_objs.append(
+                Ticket(
+                    order=order,
+                    movie_session=MovieSession.objects.get(pk=ticket_data["movie_session"]),
+                    row=ticket_data["row"],
+                    seat=ticket_data["seat"]
+                )
+            )
+
+        Ticket.objects.bulk_create(tickets_objs)
+        return order
+
+
+def get_orders(username: str = None):
+
+    qs = Order.objects.all()
+    if username:
+        qs = qs.filter(user__username=username)
+    return qs
