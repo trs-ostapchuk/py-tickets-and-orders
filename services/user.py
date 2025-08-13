cc438a5f5cd67ea38fcbd3ade78cec17ff40901a
Taras <tarasostapchuk27@gmail.com>
2025-08-13 19:49:27 +0200

    Create method create_user in user.py

---
+++ services/user.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from django.db.models import QuerySet
+
+from db.models import User
+
+
+def create_user(
+    username: str,
+    password: str,
+    email: str = None,
+    last_name: str = None,
+    first_name: str = None,
+) -> User:
+    # User = get_user_model()
+
+    extra_fields = {}
+    if email:
+        extra_fields["email"] = email
+    if first_name:
+        extra_fields["first_name"] = first_name
+    if last_name:
+        extra_fields["last_name"] = last_name
+
+    user = User.objects.create_user(
+        username=username,
+        password=password,
+        **extra_fields
+    )
+    return user
