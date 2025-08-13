69ed3a65583d07aa45a192426058e1647d7e4c1a
Taras <tarasostapchuk27@gmail.com>
2025-08-13 20:09:28 +0200

    Create method update_user

--- services/user.py
+++ services/user.py
@@ -31,3 +31,28 @@ def create_user(
 
 def get_user(user_id: int) -> User:
     return User.objects.get(id=user_id)
+
+
+def update_user(
+    user_id: int,
+    username: str = None,
+    password: str = None,
+    email: str = None,
+    last_name: str = None,
+    first_name: str = None,
+) -> User:
+    user = User.objects.get(pk=user_id)
+
+    if username is not None:
+        user.username = username
+    if email is not None:
+        user.email = email
+    if first_name is not None:
+        user.first_name = first_name
+    if last_name is not None:
+        user.last_name = last_name
+    if password is not None:
+        user.set_password(password)
+
+    user.save()
+    return user
